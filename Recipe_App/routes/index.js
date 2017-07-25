745b953080a0b8bce93e7a6b93572522a0ba4052
Cyril Kalikin <dck5132@gmail.com>
2017-07-25 19:44:24 +0000

    Updated gitignore, added background color to css, modified index.js (favicon included)

--- Recipe_App/routes/index.js
+++ Recipe_App/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Cyril's recipe site" });
 });
 
 module.exports = router;
