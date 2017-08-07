ff5300148b2de68e2c761c9df24ecffafa0ad20b
Cyril Kalikin <dck5132@gmail.com>
2017-08-07 20:55:19 +0000

    First Commit of Recipe Site - This will use Angular 4, Bootstrap 4, NgBootstrap, Node, Express, JS, HTML, CSS, JSON, TS

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    NgbModule.forRoot(),BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
